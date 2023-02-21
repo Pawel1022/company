358e5837ee0aff81df05073a968e08ab8f906dad
Pawel Wozniak <pawel.wozniak200116@gmail.com>
2023-02-21 17:17:56 +0100

    first look mobile

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const innerList = document.querySelector('.nav__list--inner')
+const btnOffers = document.querySelector('#btn')
+const navBtn = document.querySelector('.nav__btn')
+const navBox = document.querySelector('.nav__box')
+const body = document.body
+
+btnOffers.addEventListener('click', () => {
+	innerList.classList.toggle('nav__list--active')
+})
+
+navBtn.addEventListener('click', () => {
+	navBox.classList.toggle('nav__box--active')
+	navBox.classList.contains('nav__box--active') ? (body.style.overflowY = 'hidden') : (body.style.overflowY = 'visible')
+    
+})
